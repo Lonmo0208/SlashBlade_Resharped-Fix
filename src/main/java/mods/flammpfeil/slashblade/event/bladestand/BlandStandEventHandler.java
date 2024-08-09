9f0f465457e1ad9a4eace3d195b787ce19e6f6a6
Arcomit <8089341@qq.com>
2024-08-10 00:44:05 +0800

    添加使用带有附魔的耀魂物品敲击刀架台添加附魔的方式(BladeStandAttackEvent)

--- src/main/java/mods/flammpfeil/slashblade/event/bladestand/BlandStandEventHandler.java
+++ src/main/java/mods/flammpfeil/slashblade/event/bladestand/BlandStandEventHandler.java
@@ -11,13 +11,18 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
+import net.minecraft.util.RandomSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
-import net.minecraftforge.api.distmarker.Dist;
+import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 @EventBusSubscriber()
 public class BlandStandEventHandler {
@@ -92,15 +97,43 @@ public class BlandStandEventHandler {
 		ItemStack stack = player.getItemInHand(InteractionHand.MAIN_HAND);
 		
 		ItemStack blade = event.getBlade();
-		
+
+		if (!stack.is(SBItems.proudsoul_tiny) && !stack.is(SBItems.proudsoul) && !stack.is(SBItems.proudsoul_ingot))
+			return;
+
 		if(!stack.isEnchanted())
 			return ;
-		
+
+		RandomSource random = player.getRandom();
+		Map<Enchantment, Integer> currentBladeEnchantments = EnchantmentHelper.getEnchantments(blade);
+		AtomicBoolean canEnchanted = new AtomicBoolean(false);
 		stack.getAllEnchantments().forEach((enchantment,level)->{
 			if(!blade.canApplyAtEnchantingTable(enchantment))
 				return;
-			
+
+			if (currentBladeEnchantments.containsKey(enchantment)){
+				if (currentBladeEnchantments.get(enchantment) >= enchantment.getMaxLevel())//已达该附魔的最大等级则不添加
+					return;
+				currentBladeEnchantments.put(enchantment, currentBladeEnchantments.get(enchantment) + level);
+				canEnchanted.set(true);
+			}else {
+				currentBladeEnchantments.put(enchantment, level);
+				canEnchanted.set(true);
+			}
+
 		});
-		
+		if (canEnchanted.get()){
+			EnchantmentHelper.setEnchantments(currentBladeEnchantments, blade);
+
+			player.level().playSound(event.getBladeStand(), event.getBladeStand().getPos(),
+					SoundEvents.ENCHANTMENT_TABLE_USE, SoundSource.BLOCKS, 1f, random.nextFloat() * 0.1F + 0.9F);
+			Minecraft.getInstance().particleEngine.createTrackingEmitter(event.getBladeStand(),
+					ParticleTypes.ENCHANTED_HIT);
+
+			if (!player.isCreative()){
+				stack.shrink(1);
+			}
+		}
+		event.setCanceled(true);//防止掉落拔刀
 	}
 }
