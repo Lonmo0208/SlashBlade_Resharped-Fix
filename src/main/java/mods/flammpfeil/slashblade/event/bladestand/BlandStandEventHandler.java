6eccc38629e662aa509d9a4c5c8ff806c7b9b027
Arcomit <8089341@qq.com>
2024-08-10 18:16:13 +0800

    根据用于附魔的耀魂物品决定附魔成功的概率,修复其他玩家只能听见声音无法看见粒子的BUG

--- src/main/java/mods/flammpfeil/slashblade/event/bladestand/BlandStandEventHandler.java
+++ src/main/java/mods/flammpfeil/slashblade/event/bladestand/BlandStandEventHandler.java
@@ -8,6 +8,7 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
@@ -35,7 +36,7 @@ public class BlandStandEventHandler {
 
 
 	}
-	
+
 	@SubscribeEvent
 	public static void eventChangeSA(SlashBladeEvent.BladeStandAttackEvent event) {
 		if(!(event.getDamageSource().getEntity() instanceof ServerPlayer))
@@ -52,13 +53,25 @@ public class BlandStandEventHandler {
 			return;
 
 		ItemStack blade = event.getBlade();
+
 		blade.getCapability(ItemSlashBlade.BLADESTATE).ifPresent(state -> {
 			if (!SAKey.equals(state.getSlashArtsKey())) {
 				state.setSlashArtsKey(SAKey);
+
+				RandomSource random = player.getRandom();
 				player.level().playSound(event.getBladeStand(), event.getBladeStand().getPos(),
 						SoundEvents.WITHER_SPAWN, SoundSource.BLOCKS, 1f, 1f);
-				Minecraft.getInstance().particleEngine.createTrackingEmitter(event.getBladeStand(),
-						ParticleTypes.PORTAL);
+				for(int i = 0; i < 32; ++i) {
+					double xDist = (random.nextFloat() * 2.0F - 1.0F);
+					double yDist = (random.nextFloat() * 2.0F - 1.0F);
+					double zDist = (random.nextFloat() * 2.0F - 1.0F);
+					if (!(xDist * xDist + yDist * yDist + zDist * zDist > 1.0D)) {
+						double x = event.getBladeStand().getX(xDist / 4.0D);
+						double y = event.getBladeStand().getY(0.5D + yDist / 4.0D);
+						double z = event.getBladeStand().getZ(zDist / 4.0D);
+						((ServerLevel)player.level()).sendParticles(ParticleTypes.PORTAL, x, y, z,0, xDist, yDist + 0.2D, zDist,1);
+					}
+				}
 
 				if (!player.isCreative()){
 					stack.shrink(1);
@@ -66,51 +79,59 @@ public class BlandStandEventHandler {
 			}
 		});
 		event.setCanceled(true);//防止掉落拔刀
-		
+
 	}
-	
+
 	@SubscribeEvent
 	public static void eventCopySE(SlashBladeEvent.BladeStandAttackEvent event) {
 		if(!(event.getDamageSource().getEntity() instanceof ServerPlayer))
 			return;
 		Player player = (Player) event.getDamageSource().getEntity();
 		ItemStack stack = player.getItemInHand(InteractionHand.MAIN_HAND);
-		
-		
+
+
 	}
-	
+
 	@SubscribeEvent
 	public static void eventCopySA(SlashBladeEvent.BladeStandAttackEvent event) {
 		if(!(event.getDamageSource().getEntity() instanceof ServerPlayer))
 			return;
 		Player player = (Player) event.getDamageSource().getEntity();
 		ItemStack stack = player.getItemInHand(InteractionHand.MAIN_HAND);
-		
-		
+
+
 	}
-	
+
 	@SubscribeEvent(priority = EventPriority.LOWEST)
 	public static void eventProudSoulEnchantment(SlashBladeEvent.BladeStandAttackEvent event) {
 		if(!(event.getDamageSource().getEntity() instanceof ServerPlayer))
 			return;
 		Player player = (Player) event.getDamageSource().getEntity();
 		ItemStack stack = player.getItemInHand(InteractionHand.MAIN_HAND);
-		
-		ItemStack blade = event.getBlade();
-
-		if (!stack.is(SBItems.proudsoul_tiny) && !stack.is(SBItems.proudsoul) && !stack.is(SBItems.proudsoul_ingot))
-			return;
-
 		if(!stack.isEnchanted())
 			return ;
 
+		float successProbability;
+		if (stack.is(SBItems.proudsoul_tiny)){
+			successProbability = 0.25f;
+		}else if (stack.is(SBItems.proudsoul)){
+			successProbability = 0.5f;
+		}else if (stack.is(SBItems.proudsoul_ingot)){
+			successProbability = 0.75f;
+		} else if (stack.is(SBItems.proudsoul_sphere) || stack.is(SBItems.proudsoul_crystal)
+				|| stack.is(SBItems.proudsoul_trapezohedron)) {
+			successProbability = 1f;
+		} else {
+			return;
+		}
+
+		ItemStack blade = event.getBlade();
 		RandomSource random = player.getRandom();
 		Map<Enchantment, Integer> currentBladeEnchantments = EnchantmentHelper.getEnchantments(blade);
 		AtomicBoolean canEnchanted = new AtomicBoolean(false);
 		stack.getAllEnchantments().forEach((enchantment,level)->{
 			if(!blade.canApplyAtEnchantingTable(enchantment))
 				return;
-
 			if (currentBladeEnchantments.containsKey(enchantment)){
 				if (currentBladeEnchantments.get(enchantment) >= enchantment.getMaxLevel())//已达该附魔的最大等级则不添加
 					return;
@@ -120,15 +141,26 @@ public class BlandStandEventHandler {
 				currentBladeEnchantments.put(enchantment, level);
 				canEnchanted.set(true);
 			}
-
 		});
-		if (canEnchanted.get()){
-			EnchantmentHelper.setEnchantments(currentBladeEnchantments, blade);
 
-			player.level().playSound(event.getBladeStand(), event.getBladeStand().getPos(),
-					SoundEvents.ENCHANTMENT_TABLE_USE, SoundSource.BLOCKS, 1f, random.nextFloat() * 0.1F + 0.9F);
-			Minecraft.getInstance().particleEngine.createTrackingEmitter(event.getBladeStand(),
-					ParticleTypes.ENCHANTED_HIT);
+		if (canEnchanted.get()){
+			if (random.nextFloat() <= successProbability){
+				EnchantmentHelper.setEnchantments(currentBladeEnchantments, blade);
+
+				player.level().playSound(event.getBladeStand(), event.getBladeStand().getPos(),
+						SoundEvents.ENCHANTMENT_TABLE_USE, SoundSource.BLOCKS, 1f, random.nextFloat() * 0.1F + 0.9F);
+				for(int i = 0; i < 32; ++i) {
+					double d0 = (random.nextFloat() * 2.0F - 1.0F);
+					double d1 = (random.nextFloat() * 2.0F - 1.0F);
+					double d2 = (random.nextFloat() * 2.0F - 1.0F);
+					if (!(d0 * d0 + d1 * d1 + d2 * d2 > 1.0D)) {
+						double d3 = event.getBladeStand().getX(d0 / 4.0D);
+						double d4 = event.getBladeStand().getY(0.5D + d1 / 4.0D);
+						double d5 = event.getBladeStand().getZ(d2 / 4.0D);
+						((ServerLevel)player.level()).sendParticles(ParticleTypes.ENCHANTED_HIT, d3, d4, d5,0, d0, d1 + 0.2D, d2,1);
+					}
+				}
+			}
 
 			if (!player.isCreative()){
 				stack.shrink(1);
